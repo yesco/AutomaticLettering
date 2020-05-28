33556973855c90c0941f238b1c24312363466b83
Jonas S Karlsson <jsk@yesco.org>
2020-05-29 03:14:29 +0700

    - rounder buttons - red cursor, can't make bigger :-( - random cleanups - adding TODOs - added a debug OLD s and outdated clock in right hand upper corner ticking up, it detects when a new version is available and say outdated! - indexOf instead of || - more robust example function driven by hash - added lastmodified-template.js using ./update-lastmodified it copies to WWW/lastmodified.js replacing <DATA> in the file

--- lastmodified-template.js
+++ lastmodified-template.js
@@ -1,14 +1,9 @@
 // javascript to be injected in page
 // every time you update/copy to server run
-//   cat lastmodified-template.js| sed "s/<DATE>/`date +'%s.%N'`/" > lastmodified.js
+// $ ./update-lastmodified
+
+function getLastModified() {
+    return <DATE>;
+}
 
 
-// delayed setup until browser ready
-setTimeout(()=>{
-    let lastmodified_seconds_real = <DATE>;
-    document.body.appendChild(Object.assign(document.createElement('div'), {
-	style.background: 'yellow',
-	style.fontSize: 32,
-	innertext: 'lastmodied = ' + lastmodified_seconds_real + 's',
-    }));
-}, 0);
