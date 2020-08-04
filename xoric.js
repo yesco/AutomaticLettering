c976bc7d060f4da3b8b45b6b15c9e1e9711934fb
Jonas S Karlsson <jsk@yesco.org>
2020-08-04 18:58:45 +0700

    - xoric.js converter -  arr2asci, asci2arr -  -v -v ... verbosity for debvugging default 1 -  -q quiet -  decodeTap: fixed length issue

--- xoric.js
+++ xoric.js
@@ -15,6 +15,7 @@
 //    tap = ORIC .tap (archieve)
 //    fil = fil(e) object (as below)
 //    dir = [fil, ...]
+//    new = create new files (from tap)
 // 
 //    dsk = not supported
 //
@@ -43,7 +44,20 @@ if (typeof btoa === 'undefined') {
   }
 }
     
-function xoric(from, to='txt', data) {
+function arr2asc(a) {
+  let t = '';
+  a.forEach(c=>t += String.fromCharCode(c));
+  return t;
+}
+
+function asc2arr(s) {
+  let a = new Uint8Array(s.length);
+  for(let i=0; i<s.length; i++)
+    a[i] = s.charCodeAt(i);
+  return a;
+}
+
+function xoric(from, to='txt', data, fil) {
   if (data == undefined) return;
 
   function niy() {
@@ -53,76 +67,74 @@ function xoric(from, to='txt', data) {
   function expect(typ) {
     if (typeof data !== typ) {
       if (typ === 'Array' && !Array.isArray(data))
-	throw new Error('xoric: input type mismatch');
+	throw new Error('xoric: input type mismatch, got ' + typeof data + ', expected ' + typ + ' constructor='+(typeof data==='object'?data.constructor.name:''));
     }
   }
 
-  function a2t(a) {
-    //a = new Uint8Array(a);
-    //return new TextDecoder('utf-8').decode(a);
-    let t = '';
-    a.forEach(c=>t += String.fromCharCode(c));
-    return t;
-  }
-
-  function t2a(t) {
-    let a = new Uint8Array(t.length);
-    for(let i=0; i<t.length; i++)
-      a[i] = s.charCodeAt(i);
-    return a;
-  }
-
-
   let files;
+
+  if (typeof data === 'string')
+    data = asc2arr(data);
 
   // normalize from-format => byte array
   switch(from) {
-  case 'raw': expect('Array'); break;
+  case 'raw': break;
   case 'hex': 
-    data = a2t(data).match(/.{1,2}/g).map(
+    data = arr2asc(data).match(/.{1,2}/g).map(
       xx=>parseInt(xx, 16));
     break;
   case 'b64':
-    data = btoa(a2t(data));
+    data = btoa(arr2asc(data));
     break;
   case 'lns': 
-    data = ('\n' + a2t(data)).replace(
+    data = ('\n' + arr2asc(data)).replace(
       /\n/g, (a,i)=>'\n' + i<data.length-1 ? i * 10 : '');
-    data = t2a(data);
+    data = asc2arr(data);
     // fall through to make bytes
   case 'bas':
   case 'txt':
-    //data = new TextEncoder().encode(a2t(data));
+    //data = asc2arr(data);
+    //data = new TextEncoder().encode(arr2asc(data));
     break;
-  case 'bac': expect('Array');
+  case 'bac':
     niy();
-  case 'tap': expect('Array'); {
+  case 'tap':
     files = decodeTap(data);
-    break; }
+    break;
   default: throw new Error('xoric: unknow from-format: ' + from);
   }
 
-  data = new Uint8Array(data);
+  if (xoric.verbose > 1) console.error('FROM: ', data);
 
   // beware, this only takes first file
-  if (files && to !== 'dir') {
-    data = files[0].data;
+  if (files && files.length && to !== 'dir') {
+    fil = files[0];
+    data = fil.data;
   }
+
+  data = new Uint8Array(data);
+  // update converted data
+  fil.data = data;
+
+  if (xoric.verbose > 1) console.error('FROM.arr: ', data);
+
 
   // saved byte array to following format
   switch(to) {
+  case 'new':
   case 'raw': return data;
   case 'hex': return data.map(b=>b.toString(16).padStart(2, '0')).join('');
   case 'b64': return atob(data);
   case 'bas':
-  case 'txt': return new TextDecoder('utf-8').decode(data);
+  case 'txt': return arr2asc(data);
   case 'bac': niy();
-  case 'tap': return encodeTap(name, data);
+  case 'tap': return encodeTap(fil);
   case 'u8a': return new Uint8Array(data);
   case 'dir': return files;
   default: throw new Error('xoric: unknow to-format: ' + to);
   }
 }
+xoric.verbose = 1;
 
 // returns an untyped array of 'bytes'
 // (simplier to .concat()!)
@@ -131,7 +143,11 @@ function encodeTap(fil) {
   let hiA = A >> 8;
   let loA = A & 0xff;
 
-  let E = fil.E || A + fil.data.length;
+  if (xoric.verbose > 1)
+    console.error('encodeTap.length: ', fil.data.length);
+  if (xoric.verbose > 2)
+    console.error('encodeTap.data: ', fil.data);
+  let E = fil.E || A + fil.data.length - 1;
   let hiE = E >> 8;
   let loE = E & 0xff;
 
@@ -145,18 +161,25 @@ function encodeTap(fil) {
     hiA, loA,
     0x00, // reserved2
   ]
-      .concat(new TextDecoder('utf-8').decode(fil.name))
+      .concat(Array.from(asc2arr(fil.name)))
       .concat([0]) // zero-terminated name
-      .concat(fil.data);
+      .concat(Array.from(fil.data));
+  if (xoric.verbose)
+    console.error('TAP fil.name: ', fil.name);
   // TODO: should we add extra byte???
 
-  return bytes;
+  return new Uint8Array(bytes);
 }
 
-function decodeTap(name, data) {
+function decodeTap(data) {
   let files = [];
-  while (file = parseFromTap(data, pos)) {
-    files.push(file);
+  let pos = -1, fil;
+  while (([fil, pos] = parseFromTap(data, pos)) && fil) {
+    files.push(fil);
+  }
+  if (xoric.verbose > 1) {
+    console.error('xoric.decodeTap.pos:', pos);
+    console.error('xoric.decodeTap.files:', files);
   }
   return files;
 }
@@ -171,6 +194,8 @@ function decodeTap(name, data) {
 // currently, throws error at error
 // TODO: return [undefined, 'error'] instead?
 function parseFromTap(bytes, pos=-1) {
+  // TOOD: buffer is not array but indexable...
+  if(0)
   if (typeof bytes !== 'object' ||
       !Array.isArray(bytes))
     throw new Error('parseFromTap: expect array');
@@ -178,6 +203,7 @@ function parseFromTap(bytes, pos=-1) {
   let startpos = pos;
 
   if (pos+1 >= bytes.length) {
+    if (xoric.verbose > 2) console.error('TAP.eof');
     return [undefined, undefined];
   }
 
@@ -185,6 +211,10 @@ function parseFromTap(bytes, pos=-1) {
   let header = true;
   while (1) {
     let b = bytes[++pos];
+    if (b === undefined)
+      return [undefined, 'EOF'];
+    if (xoric.verbose > 2)
+      console.error('TAP.head', b);
     if (b === 0x24) {
       break;
     } else if (b !== 0x16) {
@@ -198,8 +228,8 @@ function parseFromTap(bytes, pos=-1) {
 
   // extract header data
   let reserved1 = [bytes[++pos], bytes[++pos]];
-  let typ = bytes[++pos];
-  let styp = typ === 0x00 ? 'basic' : typ === 0x80 ? 'memory' : '';
+  let type = bytes[++pos];
+  let stype = type === 0x00 ? 'basic' : type === 0x80 ? 'memory' : '';
   let run = bytes[++pos];
   let srun = run === 0x80 ? 'run' : run === 0xc7 ? 'call' : '';
   ++pos; let E = (bytes[pos]<<8) + bytes[pos+1]; ++pos;
@@ -211,7 +241,11 @@ function parseFromTap(bytes, pos=-1) {
     name += String.fromCharCode(bytes[pos]);
   }
 
-  let data = bytes.slice(startpos+1, pos);
+  let len = E - A + 1;
+  if (xoric.verbose > 2)
+    console.log('parseFromTap.length: ', len);
+
+  let data = bytes.slice(pos+1, pos+1 + len);
   pos += data.length;
 
   let fil = {
@@ -224,10 +258,12 @@ function parseFromTap(bytes, pos=-1) {
     E,
     data,
     startpos,
-    endpos,
+    endpos: pos,
     length: data.length,
   }
 
+  if (xoric.verbose > 1)
+    console.error('parseFromTap:', fil);
   return [fil, pos + data.length];
 }
 
@@ -262,7 +298,8 @@ if (typeof require !== 'undefined') {
 
   let args = process.argv.slice(2);
   args.forEach(a=>{
-    console.error('xoric.arg: ', a);
+    if (xoric.verbose > 1)
+      console.error('xoric.arg: ', a);
 
     // txt2tap
     if (a.match(/^((raw|hex|b64|txt|bas|lns|bac|tap|fil|dir)[,2]{0,1})+$/)) {
@@ -271,11 +308,24 @@ if (typeof require !== 'undefined') {
       return;
     }
 
+    // verbose
+    if (a.match(/^\-v/)) {
+      xoric.verbose++;
+      return;
+    }
+
+    // quiet
+    if (a.match(/^\-q/)) {
+      xoric.verbose = 0;
+      return;
+    }
+
     // rename last read file (like stdin!)
     if (a.match(/^\-n/)) {
       a = a.replace(/^\-n/, '');
       if (a === '') {
-	console.error('xoric.rename (-nNewName): can not rename to empty name');
+	if (xoric.verbose > 1)
+	  console.error('xoric.rename (-nNewName): can not rename to empty name');
 	process.exit(1);
       }
       if (!files.length) {
@@ -302,31 +352,57 @@ if (typeof require !== 'undefined') {
     let run = auto===undefined ? 0x00 : type==0x00 ? 0x80 : 0xc7;
 
     let data = fs.readFileSync(a);
-    console.error('xoric.file: ', a, data.length);
+    if (xoric.verbose > 1)
+      console.error('xoric.file: ', a, data.length);
     // in case of .tap file it'll be replaced
     files.push( {
       name: a,
       type,
       run,
-      E: A && (A + data.length) || 0,
+      E: A && (A + data.length - 1) || 0,
       A,
       data,
       length: data.length,
     } );
   });
 
-  console.error('xoric.converts: ', converts);
-  console.error('xoric.files: ', files);
+  if (xoric.verbose > 1) {
+    console.error('xoric.converts: ', converts);
+    console.error('xoric.files: ', files);
+  }
 
-  let f = files[0];
-  if (!f) {
+  if (!files.length) {
     console.error('xoric: no files - exiting!');
     process.exit(1);
   }
-  let r = xoric(converts[0], converts[1], f.data);
-  console.error('----------------------------');
-  process.stdout.write(r);
+
+  function out(r, to) {
+    if (xoric.verbose > 2)
+      console.error('xoric.out.type: ', typeof r);
+    if (to === 'dir') {
+      console.log(r);
+    } else if (Array.isArray(r) && to === 'new') {
+      // array of fil!
+      r.forEach(f=>out(f, 'new'));
+    } else if (to === 'new') {
+      // create file
+      let name = r.name;
+      fs.writeFileSync(name, f);
+    } else {
+      process.stdout.write(r);
+    }
+
+    if (xoric.verbose > 1)
+      console.error('<<<--- ', r.length, 'bytes');
+  }
+  // do the conversions
+  files.forEach(f=>{
+    let r = xoric(converts[0], converts[1], f.data, f);
+    if (xoric.verbose)
+      console.error('--- processing', f.name, '>>>');
+    out(r, converts[1]);
+  });
 }
 
 
-module.exports = { xoric, atob, btoa, parseFromTap, decodeTap, encodeTap, };
+module.exports = { xoric, atob, btoa, arr2asc, asc2arr, parseFromTap, decodeTap, encodeTap, };
